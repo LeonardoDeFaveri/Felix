7d34525f0a7af8ce78997623d90d2d865a213e90
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2021-07-10 17:27:14 +0200

    Created repository

---
+++ src/main/java/it/truecloud/ddns/felix/FelixApplication.java
@@ -0,0 +1,13 @@
+package it.truecloud.ddns.felix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/truecloud/ddns/felix/FelixApplicationTests.java
@@ -0,0 +1,13 @@
+package it.truecloud.ddns.felix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
